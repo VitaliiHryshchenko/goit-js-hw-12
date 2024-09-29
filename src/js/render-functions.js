749a9410c7778fe4c4e8b813b2584382e3cde6fc
Vitalii Hryshchenko <v.grischenko80@gmail.com>
2024-09-29 17:06:10 +0200

    render function completed

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -20,3 +20,66 @@ export function showLoader() {
 export function hideLoader() {
   refs.loader.classList.add('is-hidden');
 }
+export function showButton() {
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
+export function hideButton() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
+export function renderMarkup(hits) {
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `
+    <li class="gallery-card">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${webformatURL}" 
+            alt="${tags}" 
+            width="360"
+            height="152" 
+          />
+        </a>
+        <div class="discribe-box">
+          <ul class="discribe-list">
+            <li class="discribe-item">
+              <h2 class="discribe-title">Likes</h2>
+              <p class="discribe-text">${likes}</p>
+            </li>
+            <li class="discribe-item">
+              <h2 class="discribe-title">Views</h2>
+              <p class="discribe-text">${views}</p>
+            </li>
+            <li class="discribe-item">
+              <h2 class="discribe-title">Comments</h2>
+              <p class="discribe-text">${comments}</p>
+            </li>
+            <li class="discribe-item">
+              <h2 class="discribe-title">Downloads</h2>
+              <p class="discribe-text">${downloads}</p>
+            </li>
+          </ul>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+}
+export function scrollGalleryCard() {
+  const gallaryCardHeight = document
+    .querySelector('.gallery-card')
+    .getBoundingClientRect().height;
+  window.scrollBy({
+    top: Math.ceil(gallaryCardHeight * 2),
+    behavior: 'smooth',
+  });
+}
